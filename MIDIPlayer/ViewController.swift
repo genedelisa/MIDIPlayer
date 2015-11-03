604beb9862fa5bf1481b9f4c7128cb3d7e9b4b8e
Gene De Lisa <gene@rockhoppertech.com>
2015-11-03 11:28:16 -0500

    Updated to Swift 2

--- MIDIPlayer/ViewController.swift
+++ MIDIPlayer/ViewController.swift
@@ -6,20 +6,35 @@
 //  Copyright (c) 2014 Gene De Lisa. All rights reserved.
 //
 
+// updated to Swift 2
+
 import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
-    var mp:AVMIDIPlayer?
+    
+    // this one is from a midi file
+    var midiPlayer:AVMIDIPlayer?
+    
+    // this one is from a sequence turned into data
+    var midiPlayerFromData:AVMIDIPlayer?
+    
     var musicPlayer:MusicPlayer?
     var soundbank:NSURL?
     let soundFontMuseCoreName = "GeneralUser GS MuseScore v1.442"
     let gMajor = "sibeliusGMajor"
-    let nightBaldMountain = "ntbldmtn" 
+    let nightBaldMountain = "ntbldmtn"
+    
+    var musicSequence:MusicSequence!
+    
     @IBOutlet var slider: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.musicSequence = createMusicSequence()
+        createAVMIDIPlayer(self.musicSequence)
+        createAVMIDIPlayerFromMIDIFIle()
+        self.musicPlayer = createMusicPlayer(musicSequence)
     }
     
     override func didReceiveMemoryWarning() {
@@ -28,58 +43,93 @@ class ViewController: UIViewController {
     
     
     @IBAction func playData(sender: AnyObject) {
-        midiData()
+        self.midiPlayerFromData?.play({ () -> Void in
+            print("finished")
+            self.midiPlayerFromData?.currentPosition = 0
+        })
     }
     
     @IBAction func play(sender: AnyObject) {
-        midiFile()
+        self.midiPlayer?.play({ () -> Void in
+            print("finished")
+            self.midiPlayer?.currentPosition = 0
+        })
     }
     
     @IBAction func playMusicSequence(sender: AnyObject) {
-        playSequence()
+        playMusicPlayer()
     }
     
     @IBAction func stopPlaying(sender: AnyObject) {
-        if let player = self.mp {
+        if let player = self.midiPlayer {
+            if player.playing {
+                player.stop()
+            }
+        }
+        if let player = self.midiPlayerFromData {
             if player.playing {
                 player.stop()
             }
         }
     }
     
-    // nope. the player crashes with this on play
-    func completion() -> Void {
-    }
     
-    func midiFile() {
-        var completion:AVMIDIPlayerCompletionHandler = {
-            println("done")
+    func createAVMIDIPlayer(musicSequence:MusicSequence) {
+        
+        guard let bankURL = NSBundle.mainBundle().URLForResource("GeneralUser GS MuseScore v1.442", withExtension: "sf2") else {
+            fatalError("\"GeneralUser GS MuseScore v1.442.sf2\" file not found.")
         }
         
-        if let contents = NSBundle.mainBundle().URLForResource(gMajor, withExtension: "mid") {
-//            self.soundbank = NSBundle.mainBundle().URLForResource(soundFontMuseCoreName, withExtension: "sf2")
-            self.soundbank = NSBundle.mainBundle().URLForResource("gs_instruments", withExtension: "dls")
-            println("soundbank \(soundbank!)")
-            if self.soundbank != nil {
-                var error:NSError?
-                self.mp = AVMIDIPlayer(contentsOfURL: contents, soundBankURL: soundbank!, error: &error)
-                if(self.mp != nil) {
-                    mp!.prepareToPlay()
-                    // crashes if you set a completion handler
-                    setupSlider()
-                    mp!.play(nil)
-                } else {
-                    if let e = error {
-                        println("Error \(e.localizedDescription)")
-                    }
-                }
-            }
+        
+        var status = OSStatus(noErr)
+        var data:Unmanaged<CFData>?
+        status = MusicSequenceFileCreateData (musicSequence,
+            MusicSequenceFileTypeID.MIDIType,
+            MusicSequenceFileFlags.EraseFile,
+            480, &data)
+        
+        if status != OSStatus(noErr) {
+            print("bad status \(status)")
         }
+        
+        if let md = data {
+            let midiData = md.takeUnretainedValue() as NSData
+            do {
+                try self.midiPlayerFromData = AVMIDIPlayer(data: midiData, soundBankURL: bankURL)
+                print("created midi player with sound bank url \(bankURL)")
+            } catch let error as NSError {
+                print("nil midi player")
+                print("Error \(error.localizedDescription)")
+            }
+            data?.release()
+            
+            self.midiPlayerFromData?.prepareToPlay()
+        }
+        
+    }
+    
+    func createAVMIDIPlayerFromMIDIFIle() {
+        
+        guard let midiFileURL = NSBundle.mainBundle().URLForResource("sibeliusGMajor", withExtension: "mid") else {
+            fatalError("\"sibeliusGMajor.mid\" file not found.")
+        }
+        guard let bankURL = NSBundle.mainBundle().URLForResource("GeneralUser GS MuseScore v1.442", withExtension: "sf2") else {
+            fatalError("\"GeneralUser GS MuseScore v1.442.sf2\" file not found.")
+        }
+        
+        do {
+            try self.midiPlayer = AVMIDIPlayer(contentsOfURL: midiFileURL, soundBankURL: bankURL)
+            print("created midi player with sound bank url \(bankURL)")
+        } catch let error as NSError {
+            print("Error \(error.localizedDescription)")
+        }
+        
+        self.midiPlayer?.prepareToPlay()
+        setupSlider()
     }
     
     func playWithMusicPlayer() {
-        var sequence = createMusicSequence()
-        self.musicPlayer = createMusicPlayer(sequence)
+        self.musicPlayer = createMusicPlayer(self.musicSequence)
         playMusicPlayer()
     }
     
@@ -88,94 +138,55 @@ class ViewController: UIViewController {
         var status = OSStatus(noErr)
         status = NewMusicPlayer(&musicPlayer)
         if status != OSStatus(noErr) {
-            println("bad status \(status) creating player")
+            print("bad status \(status) creating player")
         }
         status = MusicPlayerSetSequence(musicPlayer, musicSequence)
         if status != OSStatus(noErr) {
-            println("setting sequence \(status)")
+            print("setting sequence \(status)")
         }
         status = MusicPlayerPreroll(musicPlayer)
         if status != OSStatus(noErr) {
-            println("prerolling player \(status)")
+            print("prerolling player \(status)")
         }
         return musicPlayer
     }
     
     func playMusicPlayer() {
         var status = OSStatus(noErr)
-        var playing:Boolean = 0
+        var playing:DarwinBoolean = false
         status = MusicPlayerIsPlaying(musicPlayer!, &playing)
-        if playing != 0 {
-            println("music player is playing. stopping")
+        if playing != false {
+            print("music player is playing. stopping")
             status = MusicPlayerStop(musicPlayer!)
             if status != OSStatus(noErr) {
-                println("Error stopping \(status)")
+                print("Error stopping \(status)")
                 return
             }
         } else {
-            println("music player is not playing.")
+            print("music player is not playing.")
         }
         
         status = MusicPlayerSetTime(musicPlayer!, 0)
         if status != OSStatus(noErr) {
-            println("setting time \(status)")
+            print("setting time \(status)")
             return
         }
         
         status = MusicPlayerStart(musicPlayer!)
         if status != OSStatus(noErr) {
-            println("Error starting \(status)")
+            print("Error starting \(status)")
             return
         }
     }
-
     
-    func midiData() {
-        if let contents = NSBundle.mainBundle().URLForResource(nightBaldMountain, withExtension: "mid") {
-            self.soundbank = NSBundle.mainBundle().URLForResource(soundFontMuseCoreName, withExtension: "sf2")
-            if self.soundbank != nil {
-                var data = NSData(contentsOfURL: contents)
-                var error:NSError?
-                self.mp = AVMIDIPlayer(data:data, soundBankURL: soundbank!, error: &error)
-                if(self.mp != nil) {
-                    mp!.prepareToPlay()
-                    setupSlider()
-                    mp!.play(nil)
-                } else {
-                    if let e = error {
-                        println("Error \(e.localizedDescription)")
-                    }
-                }
-            }
-        }
-        
-    }
     
-    func playSequence() {
-        var sequence = createMusicSequence()
-        var data = seqToData(sequence)
-        self.soundbank = NSBundle.mainBundle().URLForResource(soundFontMuseCoreName, withExtension: "sf2")
-        if self.soundbank != nil {
-            var error:NSError?
-            self.mp = AVMIDIPlayer(data:data, soundBankURL: soundbank!, error: &error)
-            if(self.mp != nil) {
-                mp!.prepareToPlay()
-                setupSlider()
-                mp!.play(nil)
-            } else {
-                if let e = error {
-                    println("Error \(e.localizedDescription)")
-                }
-            }
-        }
-    }
     
     func createMusicSequence() -> MusicSequence {
         // create the sequence
         var musicSequence:MusicSequence = MusicSequence()
         var status = NewMusicSequence(&musicSequence)
         if status != OSStatus(noErr) {
-            println("\(__LINE__) bad status \(status) creating sequence")
+            print("\(__LINE__) bad status \(status) creating sequence")
         }
         
         var tempoTrack:MusicTrack = MusicTrack()
@@ -184,10 +195,10 @@ class ViewController: UIViewController {
         }
         //MusicTrackClear(tempoTrack, 0, 1)
         if MusicTrackNewExtendedTempoEvent(tempoTrack, 0.0, 128.0) != noErr {
-            println("could not set tempo")
+            print("could not set tempo")
         }
         if MusicTrackNewExtendedTempoEvent(tempoTrack, 4.0, 256.0) != noErr {
-            println("could not set tempo")
+            print("could not set tempo")
         }
         
         
@@ -195,27 +206,27 @@ class ViewController: UIViewController {
         var track:MusicTrack = MusicTrack()
         status = MusicSequenceNewTrack(musicSequence, &track)
         if status != OSStatus(noErr) {
-            println("error creating track \(status)")
+            print("error creating track \(status)")
         }
         
         // bank select msb
         var chanmess = MIDIChannelMessage(status: 0xB0, data1: 0, data2: 0, reserved: 0)
         status = MusicTrackNewMIDIChannelEvent(track, 0, &chanmess)
         if status != OSStatus(noErr) {
-            println("creating bank select event \(status)")
+            print("creating bank select event \(status)")
         }
         // bank select lsb
         chanmess = MIDIChannelMessage(status: 0xB0, data1: 32, data2: 0, reserved: 0)
         status = MusicTrackNewMIDIChannelEvent(track, 0, &chanmess)
         if status != OSStatus(noErr) {
-            println("creating bank select event \(status)")
+            print("creating bank select event \(status)")
         }
         
         // program change. first data byte is the patch, the second data byte is unused for program change messages.
         chanmess = MIDIChannelMessage(status: 0xC0, data1: 0, data2: 0, reserved: 0)
         status = MusicTrackNewMIDIChannelEvent(track, 0, &chanmess)
         if status != OSStatus(noErr) {
-            println("creating program change event \(status)")
+            print("creating program change event \(status)")
         }
         
         // now make some notes and put them on the track
@@ -228,7 +239,7 @@ class ViewController: UIViewController {
                 duration: 1.0 )
             status = MusicTrackNewMIDINoteEvent(track, beat, &mess)
             if status != OSStatus(noErr) {
-                println("creating new midi note event \(status)")
+                print("creating new midi note event \(status)")
             }
             beat++
         }
@@ -239,19 +250,8 @@ class ViewController: UIViewController {
     }
     
     
-    func seqToData(musicSequence:MusicSequence) -> NSData {
-        var status = OSStatus(noErr)
-        var data:Unmanaged<CFData>?
-        status = MusicSequenceFileCreateData (musicSequence,
-            MusicSequenceFileTypeID(kMusicSequenceFile_MIDIType),
-            MusicSequenceFileFlags(kMusicSequenceFileFlags_EraseFile),
-            480, &data)
-        
-        var ns:NSData = data!.takeRetainedValue()
-        return ns
-    }
     
-    // slider frobs from here to the end
+    // slider frobs from here to the end. just for the the file player.
     
     var sliderTimer:NSTimer?
     
@@ -262,16 +262,16 @@ class ViewController: UIViewController {
             userInfo:nil,
             repeats:true)
         
-        if let duration = mp?.duration {
+        if let duration = midiPlayer?.duration {
             slider.maximumValue = Float(duration)
             slider.value = 0.0
-            println("duration \(duration)")
+            print("duration \(duration)")
         }
         slider.addTarget(self, action:"sliderChanged:", forControlEvents:.ValueChanged)
     }
     
     func updateSlider() {
-        if let player = self.mp {
+        if let player = self.midiPlayer {
             slider.value = Float(player.currentPosition)
         }
     }
@@ -280,7 +280,7 @@ class ViewController: UIViewController {
     @IBAction func sliderChanged(sender: UISlider) {
         sliderTimer?.invalidate()
         
-        if let player = self.mp {
+        if let player = self.midiPlayer {
             player.stop()
             player.currentPosition = NSTimeInterval(sender.value)
             player.prepareToPlay()
